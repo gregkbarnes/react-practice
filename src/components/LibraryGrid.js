9371eaf91ea8a6a03add65313d7431dea4d2a089
Gregory Barnes <gregkbarnes@gmail.com>
2017-02-21 23:28:04 -0800

    Started moving component states to LibraryGrid

--- src/components/LibraryGrid.js
+++ src/components/LibraryGrid.js
@@ -2,16 +2,67 @@ import React from 'react';
 import Sortmate from './Librarygrid/Sortmate';
 // import createSortmate from '../../createSortmate';
 
-var libComponents = []
+let libComponents = [];
 
 
 class LibraryGrid extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
+      grid: {midpoint: 0},
+      sortmates: Array(0),
+      conveyors: Array(0),
       cursorX: 0,
       cursorY: 0
     }
+  }
+
+  componentDidMount() {
+    //grab width of grid
+    let gridElement = document.getElementById('library-grid');
+    let gridWidth = (parseInt(window.getComputedStyle(gridElement).width.slice(0,-2)) / 2);
+    let gridHeight = (parseInt(window.getComputedStyle(gridElement).height.slice(0,-2)) / 2);
+
+    this.setState({
+      grid: {midpoint: gridWidth}
+    });
+
+    this.addSortmate(gridWidth, gridHeight)
+
+    // if (!libComponents.sortmate) {
+    //   this.addComponent();
+    // }
+  }
+
+  centerComponents() {
+    let componentXPositions = [];
+    let componentYPositions = [];
+
+    for (var i = 0; i < this.state.sortmates.length; i++) {
+      componentXPositions[i] = this.state.sortmates[i].xPosition;
+      componentYPositions[i] = this.state.sortmates[i].yPosition;
+    }
+
+    console.log('MinX=' + Math.min.apply(Math, componentXPositions));
+    console.log('MaxX=' + Math.max.apply(Math, componentXPositions));
+    console.log('MinY=' + Math.min.apply(Math, componentYPositions));
+    console.log('MaxY=' + Math.max.apply(Math, componentYPositions));
+
+    let x = Math.max.apply(Math, componentXPositions);
+    let y = Math.max.apply(Math, componentYPositions);
+  }
+
+  addSortmate(x,y){
+    console.log("Function Called: LibraryGrid.addSortmate()");
+    let mySortmate = {
+      sortmateNumber: (1 + this.state.sortmates.length),
+      xPosition: x,
+      yPosition: y
+    }
+
+    this.state.sortmates.push(mySortmate);
+
+    this.centerComponents();
   }
 
   addComponent(e){
@@ -37,7 +88,7 @@ class LibraryGrid extends React.Component{
       <div className="container">
         <div id='library-grid' onMouseMove={this.updateGridCursor.bind(this)}>
           <div id='cursor-position'>{"Cursor Position: ("+this.state.cursorX+","+this.state.cursorY+")"}</div>
-          <button onClick={this.addComponent}>{'Add SortMate'}</button>
+          <button onClick={this.addSortmate.bind(this)}>{'Add SortMate'}</button>
           {libComponents}
         </div>
       </div>
--- src/components/librarygrid/Sortmate.js
+++ src/components/librarygrid/Sortmate.js
@@ -9,19 +9,15 @@ class Sortmate extends React.Component {
       xPosition: this.props.x,
       yPosition: this.props.y
     }
-
-    console.log('constructor done...');
   }
 
   componentDidMount() {
-    console.log("Create: SM" + this.state.nodeNumber + " [" + this.state.xPosition + "," + this.state.yPosition + "]");
     if (this.props.nodeNumber === '01') {
       console.log('Master sortmate found');
       this.setState({
         xPosition: 150,
         yPosition: 150
       });
-      console.log('test' + this.state.xPosition);
     }
     console.log("Create: SM" + this.state.nodeNumber + " [" + this.state.xPosition + "," + this.state.yPosition + "]");
   }
@@ -30,7 +26,7 @@ class Sortmate extends React.Component {
     return(
       <div className="sortmate">
         {/*<div className="node-label">{"SM"+this.props.nodeNumber}</div>*/}
-        {"SM"+this.props.nodeNumber+"["+this.state.xPosition+"]"}
+        {"SM"+this.props.nodeNumber}
       </div>
     )
   }
